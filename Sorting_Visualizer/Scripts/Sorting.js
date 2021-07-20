db526c173116364f88285bf41d5b844354ab7e20
MoAdelAbdelrahman <58152328+MoAdelAbdelrahman@users.noreply.github.com>
2021-07-20 06:09:41 +0200

    minimal improvments

--- Sorting_Visualizer/Scripts/Sorting.js
+++ Sorting_Visualizer/Scripts/Sorting.js
@@ -1,19 +1,35 @@
+
+/**
+ * Abstract Class 
+ *
+ * @class Sorting
+ */
 class Sorting
 {
   constructor()
-  {  
+  { 
+    if (this.constructor == Sorting) {
+      throw new Error("Abstract classes can't be instantiated.");
+    } 
+
   this.arr = [];
   this.sortedbool = false;
   this.colorsr = [];
   this.colorsg = [];
   this.colorsb = [];
-  this.arrayacess =0;
-  this.comparsions =0;
+  this.arrayacess = 0;
+  this.comparsions = 0;
   this.algorithm ="";
   this.i = 1;
   this.j = 0;
   this.n = 50;
   this.current = 0;
-  this.prevj=0;
+  this.prevj = 0;
+  
   }
+   
+  sort(){
+    throw new Error("Impelemnation not found");
+  }
+
 }
--- Sorting_Visualizer/Scripts/sketch.js
+++ Sorting_Visualizer/Scripts/sketch.js
@@ -1,8 +1,7 @@
-let is;
+let selectedAlgo;
 let sel;
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
+
+
 function setup() {
   createCanvas(500, 560);
   frameRate(100);
@@ -12,37 +11,42 @@ function setup() {
   sel.option('Bubble');
   sel.selected('Insertion');
   sel.changed(mySelectEvent);
-  is = new Insertion_sort();
-  for (let i = 0; i < is.n; i++) is.arr.push(getRandomInt(500));
+  selectedAlgo = new Insertion_sort();
 
-  current = is.arr[is.i];
+  for (let i = 0; i < selectedAlgo.n; i++) selectedAlgo.arr.push(random(height));
+  current = selectedAlgo.arr[selectedAlgo.i];
 }
 
 function draw() {
   
   background(0);
   fill(255, 255, 255);
-  text("Algorithm - " + is.algorithm +
-  ", Array access - " + is.arrayacess + ", Comparsions: "
-  + is.comparsions,0,550);
-  for (let k = 0; k < is.n; k++) {
-    fill(is.colorsr[k], is.colorsg[k], is.colorsb[k]);
-    rect((k * 500) / is.n, 0, 500 / is.n, is.arr[k]);
+  text("Algorithm - " + selectedAlgo.algorithm +
+  ", Array access - " + selectedAlgo.arrayacess + ", Comparsions: "
+  + selectedAlgo.comparsions,0,550);
+  for (let k = 0; k < selectedAlgo.n; k++) {
+    fill(selectedAlgo.colorsr[k], selectedAlgo.colorsg[k], selectedAlgo.colorsb[k]);
+    rect((k * 500) / selectedAlgo.n, 0, 500 / selectedAlgo.n, selectedAlgo.arr[k]);
   }
   // Choosing the first element in our unsorted subarray
 
   // The last element of our sorted subarray
-  is.sort();
+  selectedAlgo.sort();
 }
 
 function mySelectEvent() {
+
+  //combo box
   let item = sel.value();
   fill(255, 255, 255);
-  text(item,0,520);
-  if(item == 'Bubble')
-  is = new Bubble_sort();
-  else if(item=='Insertion')
-  is=new  Insertion_sort();
+  text(item, 0, 520);
 
-  for (let i = 0; i < is.n; i++) is.arr.push(getRandomInt(500));
+  //algorithm selection
+  if (item == 'Bubble')
+  selectedAlgo = new Bubble_sort();
+  else if(item=='Insertion')
+  selectedAlgo = new  Insertion_sort();
+
+  //pushing random values
+  for (let i = 0; i < selectedAlgo.n; i++) selectedAlgo.arr.push(random(height));
 }
