3a5da4f941bb3e912b4276ab6a52974ea09df09b
MoAdelAbdelrahman <58152328+MoAdelAbdelrahman@users.noreply.github.com>
2021-07-25 20:27:20 +0200

    Selection Sort, Initial Web design, and Overall Structure refactoring.

--- Sorting_Visualizer/Scripts/Selection_Sort.js
+++ Sorting_Visualizer/Scripts/Selection_Sort.js
@@ -1,62 +1,72 @@
 class Selection_Sort extends Sorting {
     constructor() {
       super();
-      let minElement = this.j;
-      for (let k = 0; k < this.n; k++) {
-        this.colorsr.push(0);
-        this.colorsg.push(0);
-        this.colorsb.push(150);
-      }
+      this.x = this.n-1;
+      this.setInitialColors(255, 255, 255);
       this.algorithm = "Selection Sort";
     }
 
     
-    sort() {
-      //this.setInitialColors();
-      if (this.j == this.n - 1 || this.sortedbool == true) {
+    async sort() {
+      if (this.j == this.n || this.sortedbool == true) {
         this.sortedbool = true;
-        this.sorted();
-      } 
-      else {
-          //find min
-        minElement = this.j;
-        if (this.arr[this.i] < this.arr[minElement])
-               Text("inside if", 250,250);
-               minElement = this.i;
-               this.setIndexColor(this, minElement, 155 , 255, 110);
-               this.comparsions++;
-               this.arrayacess += 2;
-               this.i++;
-        //swap     
-        
-        this.swap(this.arr, minElement, this.j);
-        this.arrayacess += 3;       
-        this.j++;
+        await this.sleep(1300);
+        this.greenOnFinish();
       }
+      else 
+      { 
+        this.ColorManager();
 
+        if (this.j < this.n - 1){
+        this.next = this.j + 1;
+        this.minElement = this.j;
+        //get min
+        while (this.next < this.n){
+            if (this.arr[this.next] < this.arr[this.minElement])
+                 this.minElement = this.next;
+                  this.comparsions += 2;
+                  this.arrayacess += 2;
+                  this.OnSwapColors();
+           this.next++;                
+        }
+        //swap
+        this.swap(this.minElement, this.j);     
       }
+      this.j++;
+    }
+
+    }
     
     
- swap(xp, yp)
+ swap(a, b)
 {
-    var temp = this.arr[xp];
-    this.arr[xp] = this.arr[yp];
-    this.arr[yp] = this.temp;
+  this.temp = this.arr[a];
+  this.arr[a] = this.arr[b];
+  this.arr[b] = this.temp;
 }
 
 
-setInitialColors(Sorting){
-    selectedAlgo.colorsr[this.j + 1] = 255;
-      selectedAlgo.colorsg[this.j + 1] = 0;
-      selectedAlgo.colorsb[this.j + 1] = 0;
-      selectedAlgo.colorsr[this.prevj] = 255;
-      selectedAlgo.colorsg[this.prevj] = 255;
-      selectedAlgo.colorsb[this.prevj] = 255;
-      selectedAlgo.colorsr[this.n - this.i + 1] = 0;
-      selectedAlgo.colorsg[this.n - this.i + 1 ] = 255;
-      selectedAlgo.colorsb[this.n - this.i +1] = 0;
+ColorManager(){
+      selectedAlgo.colorsr[this.j] = 255;
+      selectedAlgo.colorsg[this.j] = 0;
+      selectedAlgo.colorsb[this.j] = 0;
+      this.colorsr[this.next] = 214;
+      this.colorsg[this.next] = 78;
+      this.colorsb[this.next] = 9;
 }
 
-    
+OnSwapColors() {
+  this.colorsr[this.minElement] = 23;
+  this.colorsg[this.minElement] = 192;
+  this.colorsb[this.minElement] = 195;
+  this.colorsr[this.next] = 20;
+  this.colorsg[this.next] = 194;
+  this.colorsb[this.next] = 97;
+}
+
+ sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
   }
-  
