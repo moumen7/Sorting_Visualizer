22c5273801f2eaa320232c9e1423137bd3c9df5c
moumen7 <moumenhamada30@gmail.com>
2021-09-17 16:38:31 +0200

    Modified the speed slider, Unified colors and some refactoring

--- Sorting_Visualizer/Scripts/Selection_Sort.js
+++ Sorting_Visualizer/Scripts/Selection_Sort.js
@@ -4,7 +4,6 @@ class Selection_Sort extends Sorting {
       this.x = this.n-1;
       this.next = this.j + 1;
       this.prevMin = 0;
-      this.setInitialColors(0, 0, 150);
       this.algorithm = "Selection Sort";
       this.min_idx = 0;
       this.points = false;
@@ -14,27 +13,18 @@ class Selection_Sort extends Sorting {
     }
     
 
-    sort(){
-
-      if(this.finish)
-      {
-        this.ColornOnFinish(0, 255, 0);
-      }
-      else
-      { 
-        this.preformsort();
-      }
+    async sort(){
+        await this.preformsort();
+        await this.sorted()
     }
 
 
     async preformsort()
     {
-        this.SelctionSort();
+        await this.SelctionSort();
     }
 
      async SelctionSort() {
-
-      
       for (let k  = 0; k < this.n-1; k++)
       {
         //being sorted now
@@ -42,10 +32,7 @@ class Selection_Sort extends Sorting {
         this.min_idx = k;
         let min = await this.getMin(k);
         this.states[min] = 0;
-        if (!this.points)
-          await this.sleep(100);
-        if (this.points)
-          await this.sleep(25);
+        await this.sleep(this.sleepfactor);
         this.swap(min, k);
         this.states[k] = -1;
         this.states[min] = -1;
@@ -55,31 +42,30 @@ class Selection_Sort extends Sorting {
     
 
 
-
-
-
 async getMin(k){
   for (let c = k + 1; c < this.n; c++){
+    this.states[c] = 0;
     if (this.arr[c] < this.arr[this.min_idx])
+    {
+      if(this.min_idx!=k)
+      {
+        this.states[this.min_idx] = -1;
+      }
         this.min_idx = c;
-        this.comparsions += 1;
-        this.arrayacess += 2;
+        this.states[this.min_idx] = 0;
     }
+
+    this.comparsions += 1;
+    this.arrayacess += 2;
+    if (!this.points)
+    await this.sleep(this.sleepfactor);
+    if(c!=this.min_idx)
+      this.states[c] = -1;
+    
+    }
+    
     return this.min_idx;
 }
 
-ColorManager(k){
-  var color = 255;
-    if(this.states[k] == 0) {
-        // min element
-        return color = '#ff002f';
-    }
-    else if (this.states[k] == 1) {
-        // Sorting bar
-        return color = '#1cff51';
-    }
-    else
-      return color;
-}
 
   }
--- Sorting_Visualizer/Scripts/Sorting.js
+++ Sorting_Visualizer/Scripts/Sorting.js
@@ -1,97 +1,87 @@
-
 /**
  * Abstract Class 
  *
  * @class Sorting
  */
-class Sorting
-{
-  constructor()
-  { 
-    if (this.constructor == Sorting) {
-      throw new Error("Abstract classes can't be instantiated.");
-    } 
-
-  this.arr = [];
-  this.sortedbool = false;
-  this.colorsr = [];
-  this.colorsg = [];
-  this.colorsb = [];
-  this.arrayacess = 0;
-  this.comparsions = 0;
-  this.points = false;
-  this.algorithm ="";
-  this.i = 0;
-  this.j = 0;
-  this.n = 200;
-  this.current = 0;
-  this.prevj = 0;
-  this.x = this.n - 1;
-  this.blinker = 0;
-  this.states = [];
-  
-  }
+ class Sorting
+ {
+   constructor()
+   { 
+     if (this.constructor == Sorting) {
+       throw new Error("Abstract classes can't be instantiated.");
+     } 
+ 
+   this.arr = [];
+   this.sortedbool = false;
+   this.colorsr = [];
+   this.colorsg = [];
+   this.colorsb = [];
+   this.arrayacess = 0;
+   this.comparsions = 0;
+   this.points = false;
+   this.algorithm ="";
+   this.i = 0;
+   this.j = 0;
+   this.n = 200;
+   this.current = 0;
+   this.prevj = 0;
+   this.sleepfactor=25;
+   this.x = this.n - 1;
+   this.blinker = 0;
+   this.states = [];
    
-  async sort(){
-    throw new Error("Impelemenation not found");
-  }
-  
-  ColorManager(){
-    throw new Error("Impelemenation not found");
-  }
-
-  preformsort() {
-    throw new Error("Implementaion not found");
-  }
-
-  sorted(){
-    throw new Error("Implementaion not found");
-  }
-
-  endsearch(){
-    throw new Error("Implementaion not found");
-  }
-
-  setInitialColors(r, g ,b)
-  {
-    for (let k = 0; k < this.n; k++) {
-      this.colorsr.push(r);
-      this.colorsg.push(g);
-      this.colorsb.push(b);
-    }
-  }
-
-  ColornOnFinish(r, g, b){
-      this.setIndexColor(this.x, r, g, b);
-      this.x--;
-  }
-
-  setIndexColor(index, r, g, b) {
-        this.colorsr[index] = r;
-        this.colorsg[index] = g;
-        this.colorsb[index] = b;
-  }
-
-  
-  blink(index, r ,g ,b){
-    this.blinker++;
-
-  if (this.blinker % 5 === 0)
-  	  this.setIndexColor(index, r,g,b);
-  else
-      this.setIndexColor(index, 0,0, 150);
-  }
-
-  async swap(a, b)
-  {
-    
-    this.temp = this.arr[a];
-    this.arr[a] = this.arr[b];
-    this.arr[b] = this.temp;
-    this.arrayacess+=4;
-  }
-  sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
    }
-  
-}
+    
+   async sort(){
+     throw new Error("Impelemenation not found");
+   }
+   
+   ColorManager(){
+     throw new Error("Impelemenation not found");
+   }
+ 
+   preformsort() {
+     throw new Error("Implementaion not found");
+   }
+ 
+   async sorted(){
+    for (let k = 0; k < this.n; k++) {
+      this.states[k] = 2;
+      await this.sleep(10);
+    }
+   }
+ 
+   endsearch(){
+     throw new Error("Implementaion not found");
+   }
+   ColorManager(k)
+   {
+      var color = 255;
+       if(this.states[k] == 0) {
+           // Pivot Element
+           return color = '#ff002f';
+       }
+       else if (this.states[k] == 1) {
+           // Sorting bar
+           return color = '#90ee90';
+       }
+       else if (this.states[k] == 2) {
+         // Sorting bar
+         return color = '#1cff51';
+     }
+       else
+         return color;
+ }
+   async swap(a, b)
+   {
+     
+     this.temp = this.arr[a];
+     this.arr[a] = this.arr[b];
+     this.arr[b] = this.temp;
+     this.arrayacess+=4;
+   }
+   async sleep(ms) {
+     return new Promise(resolve => setTimeout(resolve, ms));
+    }
+   
+ }
